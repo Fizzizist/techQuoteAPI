1a906acdcb735f036d57d19c4dafc32c5d9271ad
root <root@groupstack.localdomain>
2018-03-12 04:11:32 +0000

    testing

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var http = require('http');
 var json = require('json-object');
+var mysql = require('mysql');
+var newJson;
+
+var con = mysql.createConnection({
+	host : "localhost",
+	user : "techUser",
+	password : "techisgreat",
+	database : "TechQuotes"
+});
 
 http.createServer(function(req,res){
-	var newJson = {
-		"quote": "hello world",
-		"author":"Peter"
-	}
-	//res.setHeader(200, {'Content-Type': 'application/json'});
-	res.end(JSON.stringify(newJson));
+		res.setHeader(200, {'Content-Type': 'application/json'});
+		con.query("SELECT * FROM quotes", function (err, result, fields){
+			if (err) throw err;
+			var randomNum = Math.floor(Math.random()*10);
+			newJson = {
+				"quote" : result[randomNum].quote,
+				"author" : result[randomNum].author
+			}
+			console.log(result);
+			res.end(JSON.stringify(newJson));
+
+		});
 }).listen(3000, 'localhost');
